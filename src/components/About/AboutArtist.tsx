d010981208f5d60dafdb517041175610ce8afa91
Karthik Kumar <jkkr.1996@gmail.com>
2021-04-24 18:37:45 +0530

    fix: Image Rendering check for About page

--- src/components/About/AboutArtist.tsx
+++ src/components/About/AboutArtist.tsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import "./AboutArtist.scss";
 
 export const AboutArtist = () => {
+  const [isAssetLoaded, setAssetLoad] = useState(false)
   return (
-    <div className="about-section">
-      <img src="./assets/images/anjali-bio.jpg" alt="Anjali-Bio" />
+    <div className={`about-section ${isAssetLoaded ? 'show' : 'hide'}`}>
+      <img src="./assets/images/anjali-bio.jpg" alt="Anjali-Bio" onLoad={() => setAssetLoad(true)} />
       <div className="quote-section">
         <blockquote>
           The man who comes back through the door in the wall will never be
